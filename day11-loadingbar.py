477a7fd699775adcd5febef57ac55acccae91c35
Anonymous One <anonymousone@Anonymous-MB-Pro.local>
2019-02-17 02:18:48 -0500

    Loading Bar For CLI

---
+++ day11-loadingbar.py
@@ -0,0 +1,18 @@
+'''
+Function:       Loading Bar for Command Line
+Date:           02.17.2019
+Created By:     Anonymous Systems
+Dependencies:   tqdm
+'''
+# from __future__ import print_function
+import time
+from tqdm import tqdm
+
+
+def startLoadingBar():
+    for i in tqdm(range(10000)):
+        time.sleep(0.002)
+        pass
+
+if __name__ == '__main__':
+    startLoadingBar()
